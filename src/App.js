cd8df85d018bcd050d83578dd22aca4644dcaab4
ericyounger <ericy@stud.ntnu.no>
2021-01-23 23:42:42 +0100

    removed files and restructured it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/no-children-prop */
+import "./App.css";
+import Upload from "./components/Upload";
+import Download from "./components/Download";
+import React from "react";
+import NotFound from "./components/404";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from "react-router-dom";
 
 export default App;
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" children={<Upload />} />
+				<Route exact path="/404" children={<NotFound />} />
+				<Route path="/files/:id" children={<Download />} />
+				<Route component={NotFound} />
+			</Switch>
+		</Router>
+	);
+}
